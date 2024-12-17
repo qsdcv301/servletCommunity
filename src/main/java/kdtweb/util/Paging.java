35d240da15a781e4e81f33d9135ee3a7d927e3ec
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-12-17 10:25:48 +0900

    fix:오류있던 부분 수정

--- src/main/java/kdtweb/util/Paging.java
+++ src/main/java/kdtweb/util/Paging.java
@@ -2,12 +2,12 @@ package kdtweb.util;
 
 public class Paging {
    
-	private int totalRecords;   //ì „ì²´ ê²Œì‹œê¸€ ìˆ˜
-	private int recordsPerPage;  //í•œ í˜ì´ì§€ë‹¹ ë³´ì—¬ì¤„ ê²Œì‹œê¸€ ìˆ˜
-	private int currentPage;  //í˜„ì¬ í˜ì´ì§€
-	private int totalPages;   //ì „ì²´ í˜ì´ì§€
-	private int pagesPerGroup;  //í•œ ë²ˆì— ë³´ì—¬ì¤„ í˜ì´ì§€ ë²ˆí˜¸ì˜ ìˆ˜ (ì˜ˆ:10ì´ë©´ 1~10, 11~20)  
-    private int currentGroup;  //í˜„ì¬ í˜ì´ì§€ ê·¸ë£¹
+	private int totalRecords;   //? „ì²? ê²Œì‹œê¸? ?ˆ˜
+	private int recordsPerPage;  //?•œ ?˜?´ì§??‹¹ ë³´ì—¬ì¤? ê²Œì‹œê¸? ?ˆ˜
+	private int currentPage;  //?˜„?¬ ?˜?´ì§?
+	private int totalPages;   //? „ì²? ?˜?´ì§?
+	private int pagesPerGroup;  //?•œ ë²ˆì— ë³´ì—¬ì¤? ?˜?´ì§? ë²ˆí˜¸?˜ ?ˆ˜ (?˜ˆ:10?´ë©? 1~10, 11~20)  
+    private int currentGroup;  //?˜„?¬ ?˜?´ì§? ê·¸ë£¹
 	
 	public Paging(int totalRecords, int recordsPerPage, int currentPage, int pagesPerGroup) {
 		this.totalRecords = totalRecords;
@@ -18,38 +18,38 @@ public class Paging {
 		this.currentGroup = (int) Math.ceil((double)currentPage / pagesPerGroup); 
 	}
    
-	//ë³´ì—¬ì§€ëŠ” í˜ì´ì§€ ê·¸ë£¹ì—ì„œ ì²« ë²ˆì§¸ ë²ˆí˜¸
+	//ë³´ì—¬ì§??Š” ?˜?´ì§? ê·¸ë£¹?—?„œ ì²? ë²ˆì§¸ ë²ˆí˜¸
 	public int getStartPageOfGroup() {
 		return (currentGroup - 1) * pagesPerGroup + 1;
 	}
 	
-	//ë³´ì—¬ì§€ëŠ” í˜ì´ì§€ ê·¸ë£¹ì—ì„œ ë§ˆì§€ë§‰ ë²ˆí˜¸
+	//ë³´ì—¬ì§??Š” ?˜?´ì§? ê·¸ë£¹?—?„œ ë§ˆì?ë§? ë²ˆí˜¸
 	public int getEndPageOfGroup() {
 		int endPage = currentGroup * pagesPerGroup;
 		return Math.min(endPage, totalPages);
 	}
 	
-	//í˜„ì¬ í˜ì´ì§€ì˜ ì‹œì‘ ê²Œì‹œê¸€ ë²ˆí˜¸
+	//?˜„?¬ ?˜?´ì§??˜ ?‹œ?‘ ê²Œì‹œê¸? ë²ˆí˜¸
 	public int getStartRecord() {
 	   return (currentPage -1) * recordsPerPage;	
 	}
 	
-	//ì „ì²´ ë ˆì½”ë“œìˆ˜ ê²Œí„°
+	//? „ì²? ? ˆì½”ë“œ?ˆ˜ ê²Œí„°
 	public int getTotalRecords() {
 		return totalRecords;
 	}
 	
-	//í•œ í˜ì´ì§€ë‹¹ ë³´ì—¬ì¤„ ë²ˆí˜¸ ìˆ˜ ê²Œí„°
+	//?•œ ?˜?´ì§??‹¹ ë³´ì—¬ì¤? ë²ˆí˜¸ ?ˆ˜ ê²Œí„°
 	public int getRecordsPerPage() {
 		return recordsPerPage;
 	}
 	
-	//í˜„ì¬ í˜ì´ì§€ ë²ˆí˜¸ ê²Œí„°
+	//?˜„?¬ ?˜?´ì§? ë²ˆí˜¸ ê²Œí„°
 	public int getCurrentPage() {
 		return currentPage;
 	}
 	
-	//ì „ì²´ í˜ì´ì§€
+	//? „ì²? ?˜?´ì§?
 	public int getTotalPages() {
 		return totalPages;
 	}
